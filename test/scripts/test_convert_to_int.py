0e40e60357350345bec01e87958b291752dddf3b
robinkiplangat <robyne.kiplangat@gmail.com>
2022-01-07 16:38:48 +0300

    archive defunct script files

--- test/scripts/test_convert_to_int.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from scripts.preprocessing_helpers import convert_to_int
-
-
-def test_convert_to_int():
-    return_value = convert_to_int("2,081")
-    assert isinstance(return_value, int)
-    assert return_value == 2081
