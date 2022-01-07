fd124f4dfdf28767551e339a7888b6f088b3d0e7
robinkiplangat <robyne.kiplangat@gmail.com>
2022-01-07 23:06:10 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -4,5 +4,15 @@ from setuptools import setup, find_packages
 setup(
     name='practice module tests',
     version='0.1.0',
-    packages=find_packages(include=['module_tests'],)
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    author="Robin Kiplangat"
+    install_requires=["jupyter==1.0.0",
+                        "matplotlib==3.1.1",
+                        "numpy==1.17.3",
+                        "pytest==5.2.2",
+                        "pytest-mpl==0.10",
+                        "pytest-mock==1.11.2",
+                        "scipy==1.3.1",
+                        ]
 )
