825bac98d1c00f7046d20a00ade45725bff65222
robinkiplangat <robyne.kiplangat@gmail.com>
2022-01-08 04:28:56 +0300

    amend file with install packages

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='practice module tests',
-    version='0.1.0',
+    name="practice module tests",
+    version="0.1.0",
     packages=find_packages(),
     author="Robin Kiplangat",
+    install_requires=[
+        "pytest-mpl==0.10",
+        "pytest-mock==1.11.2",
+        "scipy==1.3.1",
+        "matplotlib==3.4.2",
+        "numpy==1.20.1",
+        "pytest==6.2.5",
+        "setuptools==59.0.1",
+    ],
 )
