7d7af36eceafe180b6abf985dad470679ddd10db
robinkiplangat <robyne.kiplangat@gmail.com>
2022-01-08 03:29:58 +0300

    add preprocess function and test

--- src/scripts/preprocessing_helpers.py
+++ src/scripts/preprocessing_helpers.py
@@ -1,3 +1,5 @@
+
+
 def convert_to_int(integer_string_with_commas):
     comma_separated_parts = integer_string_with_commas.split(",")
     for i in range(len(comma_separated_parts)):
@@ -18,3 +20,19 @@ def row_to_list(row):
     if len(separated_entries) == 2 and "" not in separated_entries:
         return separated_entries
     return None
+
+raw_data_file_path = "data/"
+
+def preprocess(raw_data_file_path, clean_data_file_path):
+    with open(raw_data_file_path, "r") as input_file:
+        rows = input_file.readlines()
+    with open(clean_data_file_path, "w") as output_file:
+        for row in rows:
+            row_as_list = row_to_list(row)
+            if row_as_list is None:
+                continue
+            area = convert_to_int(row_as_list[0])
+            price = convert_to_int(row_as_list[1])
+            if area is None or price is None:
+                continue
+            output_file.write("{0}\t{1}\n".format(area, price))
--- test/scripts/test_preprocessing_helpers.py
+++ test/scripts/test_preprocessing_helpers.py
@@ -1,87 +1,133 @@
 import pytest
 import sys
-sys.path.insert(0, './src')
-from scripts.preprocessing_helpers import convert_to_int, row_to_list
+
+sys.path.insert(0, "./src")
+
+from unittest.mock import call
+from scripts.preprocessing_helpers import convert_to_int, row_to_list, preprocess
+
+
+@pytest.fixture
+def raw_and_clean_data_file(tmpdir):
+    raw_path = tmpdir.join("raw.txt")
+    clean_path = tmpdir.join("clean.txt")
+    with open(raw_path, "w") as f:
+        f.write(
+            "1,801\t201,411\n"
+            "1,767565,112\n"
+            "2,002\t333,209\n"
+            "1990\t782,911\n"
+            "1,285\t389129\n"
+        )
+    return raw_path, clean_path
+
+
+def convert_to_int_bug_free(comma_separated_integer_string):
+    return_values = {
+        "1,801": 1801,
+        "201,411": 201411,
+        "2,002": 2002,
+        "333,209": 333209,
+        "1990": None,
+        "782,911": 782911,
+        "1,285": 1285,
+        "389129": None,
+    }
+    return return_values[comma_separated_integer_string]
+
+
+class TestConvertToInt(object):
+    def test_with_no_comma(self):
+        test_argument = "756"
+        expected = 756
+        actual = convert_to_int(test_argument)
+        message = "Expected: 756, Actual: {0}".format(actual)
+        assert actual == expected, message
+
+    def test_with_one_comma(self):
+        test_argument = "2,081"
+        expected = 2081
+        actual = convert_to_int(test_argument)
+        assert actual == expected, "Expected: 2081, Actual: {0}".format(actual)
+
+    def test_with_two_commas(self):
+        test_argument = "1,034,891"
+        expected = 1034891
+        actual = convert_to_int(test_argument)
+        assert actual == expected, "Expected: 1034891, Actual: {0}".format(actual)
+
+    def test_on_string_with_incorrectly_placed_comma(self):
+        test_argument = "12,72,891"
+        expected = None
+        actual = convert_to_int(test_argument)
+        assert actual == expected, "Expected: None, Actual: {0}".format(actual)
+
+    def test_on_string_with_missing_comma(self):
+        test_argument = "178100,301"
+        expected = None
+        actual = convert_to_int(test_argument)
+        assert actual == expected, "Expected: None, Actual: {0}".format(actual)
+
+    def test_on_float_valued_string(self):
+        test_argument = "23,816.92"
+        expected = None
+        actual = convert_to_int(test_argument)
+        assert actual == expected, "Expected: None, Actual: {0}".format(actual)
 
 
 class TestRowToList(object):
     def test_on_no_tab_no_missing_value(self):  # (0, 0) boundary value
-        # Assign actual to the return value for the argument "123\n"
         actual = row_to_list("123\n")
         assert actual is None, "Expected: None, Actual: {0}".format(actual)
 
     def test_on_two_tabs_no_missing_value(self):  # (2, 0) boundary value
         actual = row_to_list("123\t4,567\t89\n")
-        # Complete the assert statement
         assert actual is None, "Expected: None, Actual: {0}".format(actual)
 
     def test_on_one_tab_with_missing_value(self):  # (1, 1) boundary value
         actual = row_to_list("\t4,567\n")
-        # Format the failure message
         assert actual is None, "Expected: None, Actual: {0}".format(actual)
 
     def test_on_no_tab_with_missing_value(self):  # (0, 1) case
-        # Assign to the actual return value for the argument "\n"
         actual = row_to_list("\n")
-        # Write the assert statement with a failure message
         assert actual is None, "Expected: None, Actual: {0}".format(actual)
 
-    def test_on_two_tabs_with_missing_value(self):  # (2, 1) case
-        # Assign to the actual return value for the argument "123\t\t89\n"
+    def test_on_two_tabs_with_missing_value(self):  # (0, 1) case
         actual = row_to_list("123\t\t89\n")
-        # Write the assert statement with a failure message
         assert actual is None, "Expected: None, Actual: {0}".format(actual)
 
     def test_on_normal_argument_1(self):
         actual = row_to_list("123\t4,567\n")
-        # Fill in with the expected return value for the argument "123\t4,567\n"
         expected = ["123", "4,567"]
         assert actual == expected, "Expected: {0}, Actual: {1}".format(expected, actual)
 
     def test_on_normal_argument_2(self):
         actual = row_to_list("1,059\t186,606\n")
         expected = ["1,059", "186,606"]
-        # Write the assert statement along with a failure message
         assert actual == expected, "Expected: {0}, Actual: {1}".format(expected, actual)
 
-class TestConvertToInt(object):
-    def test_convert_to_int(self):
-        return_value = convert_to_int("2,081")
-        assert isinstance(return_value, int)
-        assert return_value == 2081
 
-    def test_with_no_comma(self):
-        actual = convert_to_int("756")
-        assert actual == 756, "Expected: 756, Actual: {0}".format(actual)
-
-    def test_with_one_comma(self):
-        actual = convert_to_int("2,081")
-        assert actual == 2081, "Expected: 2081, Actual: {0}".format(actual)
-
-    def test_with_two_commas(self):
-        actual = convert_to_int("1,034,891")
-        assert actual == 1034891, "Expected: 2081, Actual: {0}".format(actual)
-
-    def test_on_string_with_missing_comma(self):
-        actual = convert_to_int("178100,301")
-        assert actual is None, "Expected: None, Actual: {0}".format(actual)
-
-    def test_on_string_with_incorrectly_placed_comma(self):
-        actual = convert_to_int("12,72,891")
-        assert actual is None, "Expected: None, Actual: {0}".format(actual)
-
-    def test_on_float_valued_string(self):
-        actual = convert_to_int("23,816.92")
-        assert actual is None, "Expected: None, Actual: {0}".format(actual)
-
-    def test_on_string_with_one_comma(self):
-        test_argument = "2,081"
-        expected = 2081
-        actual = convert_to_int(test_argument)
-        # Format the string with the actual return value
-        message = "convert_to_int('2,081') should return the int 2081, but it actually returned {0}".format(
-            actual
+class TestPreprocess(object):
+    def test_on_raw_data(self, raw_and_clean_data_file,mocker):
+        raw_path, clean_path = raw_and_clean_data_file
+        convert_to_int_mock = mocker.patch(
+            "scripts.preprocessing_helpers.convert_to_int",
+            side_effect=convert_to_int_bug_free,
         )
-        # Write the assert statement which prints message opytn failure
-        assert isinstance(expected, int)
-        assert actual == expected, message
+        preprocess(raw_path, clean_path)
+        assert convert_to_int_mock.call_args_list == [
+            call("1,801"),
+            call("201,411"),
+            call("2,002"),
+            call("333,209"),
+            call("1990"),
+            call("782,911"),
+            call("1,285"),
+            call("389129"),
+        ]
+        with open(clean_path, "r") as f:
+            lines = f.readlines()
+        first_line = lines[0]
+        assert first_line == "1801\t201411\n"
+        second_line = lines[1]
+        assert second_line == "2002\t333209\n"
