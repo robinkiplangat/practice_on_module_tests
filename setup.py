a6d833075f4b526a597c67d3859334c4db127a4b
robinkiplangat <robyne.kiplangat@gmail.com>
2022-01-08 04:42:39 +0300

    revise dependancies

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     packages=find_packages(),
     author="Robin Kiplangat",
     install_requires=[
-        "pytest-mpl==0.10",
-        "pytest-mock==1.11.2",
-        "scipy==1.3.1",
-        "matplotlib==3.4.2",
-        "numpy==1.20.1",
+        "pytest-mpl",
+        "pytest-mock",
+        "scipy",
+        "matplotlib",
+        "numpy",
         "pytest==6.2.5",
-        "setuptools==59.0.1",
+        "setuptools",
     ],
 )
